da8854cb52a2a51c74d45c4989c651fe706f80eb
Ник Домнин <domnin.trend@gmail.com>
2024-07-10 18:19:17 +0300

    add src

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,1 +1,4 @@
 console.log("Welcome to the Brain Games!");
+
+import youreName from "../src/cli.js";
+console.log(`Привет, ${youreName}`);
---
+++ src/cli.js
@@ -0,0 +1,4 @@
+import readlineSync from "readline-sync";
+
+const youreName = readlineSync.question("Как тебя зовут?: ");
+export default youreName;
