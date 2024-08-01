0837ccb65fb6d3c39daff0e1155832c809246217
Ник Домнин <domnin.trend@gmail.com>
2024-08-01 20:01:50 +0300

    add brain

---
+++ bin/brain-prime.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import brainPrime from '../game/brain-prime.js';
+
+brainPrime();
---
+++ game/brain-prime.js
@@ -0,0 +1,45 @@
+import readlineSync from 'readline-sync';
+import { getRandom } from '../src/general-logic.js';
+import { hello } from '../src/cli.js';
+
+const isPrime = (num) => {
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const askPrime = () => {
+  const num = getRandom(1, 100);
+  const answer = readlineSync.question(`Вопрос: ${num}
+Ответ: `);
+  let conditeon;
+  if (answer === 'Да' && isPrime(num) === true) {
+    conditeon = true;
+  } else if (answer === 'Нет' && isPrime(num) === false) {
+    conditeon = true;
+  } else {
+    conditeon = false;
+  }
+  return conditeon;
+};
+
+const brainPrime = () => {
+  const yourName = hello('Ответ "Да" если число простое "Нет" если нет');
+  for (let i = 0; i <= 2; i += 1) {
+    const action = askPrime();
+    if (action === true) {
+      console.log('Верно');
+    } else {
+      console.log(`Неверно ${yourName}`);
+      break;
+    }
+    if (i === 2) {
+      console.log(`Молодец, ${yourName}`);
+    }
+  }
+};
+
+export default brainPrime;
